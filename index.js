2dbb80b8fd95c94fdf72b6c2e38715f495c30227
naman03malhotra <naman03malhotra@gmail.com>
2020-05-22 15:49:56 +0530

    edited example with new package name

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { matchRules } from 'matchrules';
+import { matchRules } from 'match-rules';
 
 const source = {
   admin: 'user_admin',
