9a482e9e26a0873d59201d3abbf991adbe6342be
naman03malhotra <naman03malhotra@gmail.com>
2020-07-03 18:53:48 +0530

    added more examples

--- index.js
+++ index.js
@@ -10,6 +10,41 @@ const RULE = {
   admin: 'user_admin',
 }
 
+const sourceObject = {
+  enable_unique_feature: true,
+  when_user_is_admin: true,
+  age_more_than_18: 25,
+};
+
+const ENABLE_UNIQUE_FEATURE = {
+  enable_unique_feature: true,
+  when_user_is_admin: true,
+  age_more_than_18: (value, sourceObject) => value > 18,
+};
+
+const sourceObject2 = {
+  enable_unique_feature: true,
+  userData: {
+    personalData: {
+      profile: {
+        age: 18,
+        country: 'US',
+      },
+    },
+  },
+};
+
+// Rules
+const DEEP_RULE_EXAMPLE = {
+  userData: {
+    personalData: {
+      profile: {
+        country: (value, sourceObject) => value === 'US' || value === 'IN',
+      },
+    },
+  },
+};
+
 class App extends Component {
 
   render() {
@@ -18,6 +53,15 @@ class App extends Component {
         {matchRules(source, RULE)?
         <div>render admin stuff</div>:
         <div>render non - admin stuff</div>}
+
+        {matchRules(sourceObject, ENABLE_UNIQUE_FEATURE)?
+        <div>unique feature enabled</div>:
+        <div>not rendered</div>}
+
+        {matchRules(sourceObject2, DEEP_RULE_EXAMPLE)?
+        <div>deep data source</div>:
+        <div>not rendered</div>}
+
       </div>
     );
   }
