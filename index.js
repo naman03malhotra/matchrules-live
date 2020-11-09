f5c30c0cae4551cedbf0303a1c8f3f3b4e10e1b8
naman03malhotra <naman03malhotra@gmail.com>
2020-11-09 06:09:32 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import matchRules from 'match-rules';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import matchRules from "match-rules";
 
-const sourceObject0 = {
-  admin: 'user_admin',
+const sourceObjectZero = {
+  admin: "user_admin"
 };
 
 const RULE = {
-  admin: 'user_admin',
-}
+  admin: "user_admin"
+};
 
-const sourceObject1 = {
+const sourceObjectOne = {
   enable_unique_feature: true,
   when_user_is_admin: true,
-  age_more_than_18: 25,
+  age_more_than_18: 25
 };
 
 const ENABLE_UNIQUE_FEATURE = {
   enable_unique_feature: true,
   when_user_is_admin: true,
-  age_more_than_18: (value, sourceObject1) => value > 18,
+  age_more_than_18: (value, sourceObjectOne) => value > 18
 };
 
-const sourceObject2 = {
+const sourceObjectTwo = {
   enable_unique_feature: true,
   userData: {
     personalData: {
       profile: {
         age: 18,
-        country: 'US',
-      },
-    },
-  },
+        country: "US"
+      }
+    }
+  }
 };
 
 // Rules
@@ -39,52 +39,57 @@ const DEEP_RULE_EXAMPLE = {
   userData: {
     personalData: {
       profile: {
-        country: (value, sourceObject1) => value === 'US' || value === 'IN',
-      },
-    },
-  },
+        country: (value, sourceObjectOne) => value === "US" || value === "IN"
+      }
+    }
+  }
 };
-
 
 // example where you have to dynamically match the generated rule
 const itemCreatedByUser = {
-  user_id: 123,
-}
+  user_id: 123
+};
 
 const userSource = {
-  id: 123,
-}
+  id: 123
+};
 
-const DYNAMIC_USER_RULE = (itemCreatedByUserParam) => {
+const DYNAMIC_USER_RULE = itemCreatedByUserParam => {
   return {
-    id: itemCreatedByUserParam.user_id,
-  }
-}
+    id: itemCreatedByUserParam.user_id
+  };
+};
 
 class App extends Component {
-
   render() {
     return (
       <div>
-        {matchRules(sourceObject0, RULE)?
-        <div>render admin stuff</div>:
-        <div>render non - admin stuff</div>}
+        {matchRules(sourceObjectZero, RULE) ? (
+          <div>render admin stuff</div>
+        ) : (
+          <div>render non - admin stuff</div>
+        )}
 
-        {matchRules(sourceObject1, ENABLE_UNIQUE_FEATURE)?
-        <div>unique feature enabled</div>:
-        <div>not rendered</div>}
+        {matchRules(sourceObjectOne, ENABLE_UNIQUE_FEATURE) ? (
+          <div>unique feature enabled</div>
+        ) : (
+          <div>not rendered</div>
+        )}
 
-        {matchRules(sourceObject2, DEEP_RULE_EXAMPLE)?
-        <div>deep data source</div>:
-        <div>not rendered</div>}
+        {matchRules(sourceObjectTwo, DEEP_RULE_EXAMPLE) ? (
+          <div>deep data source</div>
+        ) : (
+          <div>not rendered</div>
+        )}
 
-        {matchRules(userSource, DYNAMIC_USER_RULE(itemCreatedByUser))?
-        <div>dynamic rule case rendered</div>:
-        <div>not rendered</div>}
-
+        {matchRules(userSource, DYNAMIC_USER_RULE(itemCreatedByUser)) ? (
+          <div>dynamic rule case rendered</div>
+        ) : (
+          <div>not rendered</div>
+        )}
       </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
